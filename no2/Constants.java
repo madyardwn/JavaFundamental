d0ad3187bc9b0c924282aaa7c9a33b7c9068b2fb
madyardwn <madyardwn@gmail.com>
2022-11-09 06:32:29 +0700

    Edit

--- no2/Constants.java
+++ no2/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
     double paperWidth = 8.5; 
     double paperHeight = 11; 
     System.out.println("Paper size in centimeters: " + paperWidth * CM_PER_INCH + " by " + paperHeight *    CM_PER_INCH); 
-  } 
+    System.out.println("Paper size in centimeters: " + paperWidth * Constants.CM_PER_INCH + " by " + paperHeight * Constants.CM_PER_INCH);
+    } 
 }
